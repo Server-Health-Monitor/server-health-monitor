f138e55c203dadda7e3fe5fc8318ee9f510b227e
andresmonc <jaimeamonc@gmail.com>
2021-05-05 20:23:46 -0500

    finalized health-square component

--- health-monitor-ui/src/app/components/health-square/health-square.component.ts
+++ health-monitor-ui/src/app/components/health-square/health-square.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-health-square',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./health-square.component.css']
 })
 export class HealthSquareComponent implements OnInit {
+
+  @Input() message: string;
+  @Input() isHealthy: boolean;
 
   constructor() { }
 
