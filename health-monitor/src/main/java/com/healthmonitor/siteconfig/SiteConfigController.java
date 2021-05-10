112c83067b3f05bd7293e97cb580441ab23e3e9c
andresmonc <jaimeandresmonc@gmail.com>
2021-05-09 23:10:42 -0500

    siteconfig list all endpoint all set!

--- health-monitor/src/main/java/com/healthmonitor/siteconfig/SiteConfigController.java
+++ health-monitor/src/main/java/com/healthmonitor/siteconfig/SiteConfigController.java
@@ -1,7 +1,21 @@
 package com.healthmonitor.siteconfig;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("siteconfig")
 public class SiteConfigController {
+
+    @Autowired
+    SiteConfigService siteConfigService;
+
+    @GetMapping
+    public List<SiteConfig> getAllConfigs() {
+        return siteConfigService.listAll();
+    }
 }
