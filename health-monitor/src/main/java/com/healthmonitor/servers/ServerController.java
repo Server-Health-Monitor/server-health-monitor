18716309b3205025c348bc65f12c8adb091b8337
andresmonc <jaimeamonc@gmail.com>
2021-05-21 21:58:57 -0500

    privatize

--- health-monitor/src/main/java/com/healthmonitor/servers/ServerController.java
+++ health-monitor/src/main/java/com/healthmonitor/servers/ServerController.java
@@ -24,7 +24,7 @@ public class ServerController {
     }
 
     private static class Servers {
-        public List<Server> servers;
+        private List<Server> servers;
 
         private Servers(List<Server> servers) {
             this.servers = servers;
