2f00ccc78240599c8c8f82ec222ff7872cb1c2a5
andresmonc <jaimeamonc@gmail.com>
2021-05-21 22:37:06 -0500

    push server data to local storage every minute

--- health-monitor-ui/src/app/models/server.ts
+++ health-monitor-ui/src/app/models/server.ts
@@ -1,27 +1,29 @@
-export interface Server {
-    serverId: number;
-    group: Group;
-    displayName: string;
-    uri: string;
-    application: Application;
-    proxy: Proxy;
-    status: Status;
-  }
-  export interface Group {
-    groupID: number;
-    name: string;
-  }
-  export interface Application {
-    applicationID: number;
-    name: string;
-  }
-  export interface Proxy {
-    proxyID: number;
-    host: string;
-    port: number;
-  }
-  export interface Status {
-    serverId: number;
-    statusName: number;
-  }
-  
+export interface Servers {
+  servers?: (ServersEntity)[] | null;
+}
+export interface ServersEntity {
+  serverId: number;
+  group: Group;
+  displayName: string;
+  uri: string;
+  application: Application;
+  proxy: Proxy;
+  status: Status;
+}
+export interface Group {
+  groupID: number;
+  name: string;
+}
+export interface Application {
+  applicationID: number;
+  name: string;
+}
+export interface Proxy {
+  proxyID: number;
+  host: string;
+  port: number;
+}
+export interface Status {
+  serverId: number;
+  statusName?: null;
+}
